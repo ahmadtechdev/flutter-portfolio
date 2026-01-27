179329722e0e8c8a4518fc4462dc3a979ee1dcea
unknown <ahmadtechdev@gmail.com>
2026-01-27 13:07:46 +0500

    icon issuefix

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -37,7 +37,6 @@ class StaticUtils {
     "https://img.icons8.com/metro/100/ffffff/linkedin.png",
     "https://img.icons8.com/material-rounded/200/ffffff/github.png",
     "https://img.icons8.com/external-tal-revivo-shadow-tal-revivo/200/external-level-up-your-coding-skills-and-quickly-land-a-job-logo-shadow-tal-revivo.png"
-    "https://img.icons8.com/?size=100&id=_H27C1YMyU_5&format=png&color=000000"
   ];
 
   static const List<String> socialLinks = [
@@ -47,7 +46,6 @@ class StaticUtils {
     "https://linkedin.com/in/ahmad-razaa",
     "https://github.com/ahmadtechdev",
     "https://leetcode.com/u/ahmadrazaa/"
-    "https://www.researchgate.net/profile/Ahmad-Ali-258"
   ];
 
   static const String resume =
